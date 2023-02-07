89e1ff3c941e644d4c1fb6772cf08027fd442ffa
aleks skudarnov <skudarnovfred@mail.ru>
2023-02-07 15:41:31 +0500

    one

---
+++ src/test/java/ru/netology/TestPostman.java
@@ -0,0 +1,21 @@
+package ru.netology;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestPostman {
+    @Test
+    void shouldSendReRequestBody() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("15.07.1989")
+
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("15.07.1989"));
+    }
+}
